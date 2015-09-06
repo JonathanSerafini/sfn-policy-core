74d252e5e3b1434c032e680d2b42cd531aad743b
Jonathan Serafini <jonathan@lightspeedpos.com>
2015-09-06 11:16:34 -0400

    Refactor redis for new registries and add memcache

--- lib/sparkleformation/dynamics/cache_resource.rb
+++ lib/sparkleformation/dynamics/cache_resource.rb
@@ -1,13 +1,5 @@
 
-SparkleFormation.dynamic(:redis_group) do |_name, _config = {}|
-  _config = {} if _config.nil?
-
-  nested_configs = {}
-  nested_dynamics = %w(state)
-  nested_dynamics.each do |key|
-    nested_configs[key.to_sym] = _config.delete(key.to_sym) || {}
-  end
-
+SparkleFormation.dynamic(:redis_cluster) do |_name, _config = {}|
   outputs.set!("#{_name}_id") do
     value ref!(_name)
   end
@@ -17,9 +9,10 @@ SparkleFormation.dynamic(:redis_group) do |_name, _config = {}|
   end
 
   resources.set!(_name) do
-    set_state!(nested_configs[:state])
+    type 'AWS::ElastiCache::ReplicationGroup'
+    set_state!(_config.delete(:state) || {})
 
-    registry! :default_config, :config, 
+    registry :resource_config, :config, _config,
       engine: ref!(:cache_engine),
       port:   "6379",
       num_cache_clusters:       ref!(:cache_nodes),
@@ -29,45 +22,48 @@ SparkleFormation.dynamic(:redis_group) do |_name, _config = {}|
       automatic_failover_enabled:     "false",
       replication_group_description:  registry!(:context_name)
 
-    registry! :apply_config, :config,
-      _config
-
-    type 'AWS::ElastiCache::ReplicationGroup'
-
-    properties do
-      state!(:config).each do |key, value|
-        set!(key, value)
-      end
-    end
+    registry! :resource_properties, :config
   end
 end
 
-SparkleFormation.dynamic(:cache_subnet_group) do |_name, _config={}|
-  _config = {} if _config.nil?
+SparkleFormation.dynamic(:memcache_cluster) do |_name, _config = {}|
+  outputs.set!("#{_name}_id") do
+    value ref!(_name)
+  end
 
-  nested_configs = {}
-  nested_dynamics = %w(state)
-  nested_dynamics.each do |key|
-    nested_configs[key.to_sym] = _config.delete(key.to_sym) || {}
+  outputs.set!("#{_name}_host") do
+    value attr!(_name, "ConfigurationEndpoint.Address")
   end
 
   resources.set!(_name) do
-    set_state!(nested_configs[:state])
+    type 'AWS::ElastiCache::CacheCluster'
+    set_state!(_config.delete(:state) || {})
 
-    registry! :default_config, :config, 
+    registry :resource_config, :config, _config,
+      engine: ref!(:cache_engine),
+      port:   "11211",
+      cluster_name: registry!(:context_name),
+      AZ_mode:      "cross-az",
+      num_cache_clusters:       ref!(:cache_nodes),
+      cache_node_type:          ref!(:app_instance),
+      vpc_security_group_ids:   array!(ref!(:security_group_cache)),
+      cache_subnet_group_name:  ref!(:subnet_group_cache),
+      cache_parameter_group_name:     nil
+
+    registry! :resource_properties, :config
+  end
+end
+
+SparkleFormation.dynamic(:cache_subnet_group) do |_name, _config = {}|
+  resources.set!(_name) do
+    type "AWS::ElastiCache::SubnetGroup"
+    set_state!(_config.delete(:state) || {})
+
+    registry :resource_config, :config, _config,
       description: registry!(:context_name),
       subnet_ids: registry!(:context_subnets)
-
-    registry! :apply_config, :config,
-      _config
-    
-    type "AWS::ElastiCache::SubnetGroup"
-
-    properties do
-      state!(:config).each do |key, value|
-        set!(key, value)
-      end
-    end
+   
+    registry! :resource_properties, :config
   end
 end
 
